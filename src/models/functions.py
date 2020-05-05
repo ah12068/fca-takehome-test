0e3324eb6ef41b76dc0b3eed73e8b3d0ab30fb55
ah12068 <dang.jeremy01@gmail.com>
2020-05-05 16:01:33 +0100

    added 5 feature models

--- src/models/functions.py
+++ src/models/functions.py
@@ -9,9 +9,12 @@ from constants import (
     id_col,
     target_col,
     parameter_grid,
-    cpu_count
+    cpu_count,
+    feature_coefs,
+    sampling_strats
 )
 from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.feature_selection import RFE
 from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
 from sklearn.metrics import roc_auc_score, roc_curve
 
@@ -124,12 +127,142 @@ def baseline_trainer(processed_df, algorithm, sampler, cf, name=None):
     return algorithm, metrics, fig
 
 
+def feature_select(processed_df, sampler, algorithm, n_features):
+    logger = logging.getLogger(__name__)
+
+    cols = [col for col in processed_df.columns if col not in id_col + target_col]
+
+    X = processed_df[cols]
+    y = processed_df[target_col]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.25, random_state=111)
+
+    sampled_X, sampled_y = sampling_strats[sampler].fit_sample(X_train, y_train)
+    sampled_X = pd.DataFrame(data=sampled_X, columns=cols)
+    sampled_y = pd.DataFrame(data=sampled_y, columns=target_col)
+
+    logger.info(f'Performing RFE')
+
+    model = RFE(estimator=algorithm, n_features_to_select=n_features)
+    model.fit(X_train.values, y_train.values.ravel())
+    cols_to_keep = X.columns[model.support_].tolist()
+
+    print(f'Top {n_features} Features to keep: {cols_to_keep}')
+
+    return cols_to_keep
+
+
+def train_with_feature_selection(processed_df, sampler, algorithm, n_features, cf, name=None):
+    logger = logging.getLogger(__name__)
+
+    features = feature_select(processed_df, sampler, algorithm, n_features)
+
+    X = processed_df[features]
+    y = processed_df[target_col]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.25, random_state=111)
+
+    logger.info(f'Applying sampling strategy')
+
+    sampled_X, sampled_y = sampling_strats[sampler].fit_sample(X_train, y_train)
+    sampled_X = pd.DataFrame(data=sampled_X, columns=features)
+    sampled_y = pd.DataFrame(data=sampled_y, columns=target_col)
+
+    clf = GridSearchCV(
+        estimator=algorithm,
+        param_grid=parameter_grid[name],
+        cv=10,
+        n_jobs=cpu_count,
+        verbose=True
+    )
+
+    best_model = clf.fit(sampled_X.values, sampled_y.values.ravel())
+    predictions = best_model.predict(X_test)
+    probabilities = best_model.predict_proba(X_test)
+
+    logger.info(f'Generating metrics')
+
+    if cf == "coefficients":
+        coefficients = pd.DataFrame(best_model.best_estimator_.coef_.ravel())
+        plotly_title = 'Coefficients'
+    elif cf == "features":
+        coefficients = pd.DataFrame(best_model.best_estimator_.feature_importances_)
+        plotly_title = 'Feature Importances'
+
+    column_df = pd.DataFrame(features)
+    coef_sumry = (pd.merge(coefficients, column_df, left_index=True,
+                           right_index=True, how="left"))
+    coef_sumry.columns = ["coefficients", "features"]
+    coef_sumry = coef_sumry.sort_values(by="coefficients", ascending=False)
+
+    conf_matrix = confusion_matrix(y_test, predictions)
+    model_roc_auc = roc_auc_score(y_test, predictions)
+    fpr, tpr, thresholds = roc_curve(y_test, probabilities[:, 1])
+
+    metrics = {
+        'Classification Report': classification_report(y_test, predictions),
+        'AUC Score': model_roc_auc,
+        'Confusion Matrix': conf_matrix,
+        'TPR': tpr,
+        'FPR': fpr
+    }
+    print(f"Metrics:\n{metrics['Classification Report']}\n{model_roc_auc}")
+
+    logger.info('Producing Evaluation Report')
+
+    trace1 = go.Heatmap(z=conf_matrix,
+                        x=classes,
+                        y=classes,
+                        showscale=False,
+                        colorscale="Picnic",
+                        name="matrix")
+
+    # plot roc curve
+    trace2 = go.Scatter(x=fpr, y=tpr,
+                        name=f'AUC: {model_roc_auc}',
+                        line=dict(color='rgb(22, 96, 167)', width=2))
+    trace3 = go.Scatter(x=[0, 1], y=[0, 1],
+                        line=dict(color='rgb(205, 12, 24)', width=2,
+                                  dash='dot'))
+
+    # plot coeffs
+    trace4 = go.Bar(x=coef_sumry["features"], y=coef_sumry["coefficients"],
+                    name=cf,
+                    marker=dict(color=coef_sumry["coefficients"],
+                                colorscale="Picnic",
+                                line=dict(width=.6, color="black")))
+
+    # subplots
+    fig = make_subplots(rows=2, cols=2, specs=[[{}, {}], [{'colspan': 2}, None]],
+                        subplot_titles=('Confusion Matrix',
+                                        'Receiver operating characteristic',
+                                        plotly_title))
+
+    fig.append_trace(trace1, 1, 1)
+    fig.append_trace(trace2, 1, 2)
+    fig.append_trace(trace3, 1, 2)
+    fig.append_trace(trace4, 2, 1)
+
+    fig['layout'].update(showlegend=False, title=f"{name} performance",
+                         autosize=False, height=900, width=800,
+                         plot_bgcolor='rgba(240,240,240, 0.95)',
+                         paper_bgcolor='rgba(240,240,240, 0.95)',
+                         margin=dict(b=195))
+    fig["layout"]["xaxis2"].update(dict(title="false positive rate"))
+    fig["layout"]["yaxis2"].update(dict(title="true positive rate"))
+    fig["layout"]["xaxis3"].update(dict(showgrid=True, tickfont=dict(size=10),
+                                        tickangle=90))
+    fig.layout['hovermode'] = 'x'
+    fig.show()
+
+    return algorithm, metrics, fig
+
+
 def create_report(algorithm, test_X, test_Y):
     predictions = algorithm.predict(test_X)
     probabilities = algorithm.predict_proba(test_X)
 
     model_roc_auc = roc_auc_score(test_Y, predictions)
-
 
     metrics = f'''
         Classification Report:\n{classification_report(test_Y, predictions)}\n,
@@ -209,10 +342,10 @@ def plot_report(processed_df, algorithm, test_X, test_Y, cf, name=None):
 
     return algorithm
 
+
 def plot_roc_auc(algorithms, test_X, test_Y, name=None):
     fig = go.Figure()
     for algorithm in algorithms.keys():
-
         predictions = algorithms[algorithm].predict(test_X)
         probabilities = algorithms[algorithm].predict_proba(test_X)
         model_roc_auc = roc_auc_score(test_Y, predictions)
@@ -226,7 +359,6 @@ def plot_roc_auc(algorithms, test_X, test_Y, name=None):
                             line=dict(color='rgb(205, 12, 24)', width=2,
                                       dash='dot'))
         fig.add_trace([trace1, trace2])
-
 
     fig['layout'].update(showlegend=False, title=f"{name} performance",
                          autosize=False, height=900, width=800,
---
+++ src/models/train_model_5vars.py
@@ -0,0 +1,69 @@
+import logging
+import pandas as pd
+import pickle as pkl
+from constants import (
+    baseline_classifiers,
+    feature_coefs,
+    sampling_strats
+)
+from functions import train_with_feature_selection
+import warnings
+
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+strat = 'RandomOverSampler'
+
+
+def main(sampling_strat):
+    logger = logging.getLogger(__name__)
+
+    if sampling_strat not in sampling_strats.keys():
+        raise ValueError(f'Invalid sampling strategy, only {[x for x in sampling_strats.keys()]}')
+
+    df = pd.read_csv('../../data/processed/loans_processed.csv')
+
+    logger.info(f'Training 5-feature models')
+
+    for classifier in baseline_classifiers.keys():
+        logger.info(f'Classifier: {classifier} w/ {sampling_strat}')
+        if classifier in feature_coefs:
+            algorithm, metrics, visual_report = train_with_feature_selection(
+                processed_df=df,
+                algorithm=baseline_classifiers[classifier],
+                n_features=5,
+                sampler=strat,
+                cf='coefficients',
+                name=classifier
+            )
+
+        else:
+            algorithm, metrics, visual_report = train_with_feature_selection(
+                processed_df=df,
+                algorithm=baseline_classifiers[classifier],
+                n_features=5,
+                sampler=strat,
+                cf='coefficients',
+                name=classifier
+            )
+
+        f = open(f'../../models/{classifier}_{sampling_strat}_5var_metrics.pickle', 'wb')
+        pkl.dump(metrics, f)
+        f.close()
+
+        m_out = open(f'../../models/{classifier}_{sampling_strat}_5var_model.pickle', 'wb')
+        pkl.dump(algorithm, m_out)
+        m_out.close()
+
+        visual_report.write_html(f'../../reports/figures/{classifier}_{sampling_strat}_5var_report.html')
+        logger.info(f'Plotly charts exported to reports/figures')
+
+        logger.info(f'{classifier} and metrics (pickle files) exported to models/')
+
+    return
+
+
+if __name__ == '__main__':
+    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
+
+    main(sampling_strat=strat)
